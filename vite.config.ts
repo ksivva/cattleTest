b596179f9e54e277b32433cfcfbca7218769ac40
Kamaraj Sivva <kamaraj.sivva@gmail.com>
2025-04-17 21:04:13 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/cattleTest/', // ✅ This is the key addition
   server: {
     host: "::",
     port: 8080,
